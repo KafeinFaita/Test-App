0a63cf38ea81f7a57d56a439623fe0eef96e0b1e
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2021-02-18 01:32:10 +0800

    added update function

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const User = require('./models/user')
+const methodOverride = require('method-override')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+
 
 const dbURI = "mongodb+srv://kafein:kafeinfaita@cluster0.3xefo.mongodb.net/test-db?retryWrites=true&w=majority"
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
     .then(result => app.listen(PORT))
     .catch(err => console.log(err))
 
@@ -21,13 +24,20 @@ app.get('/', (req, res) => {
     User.find().sort({ createdAt: -1 })
         .then(result => {
             res.render('index', { title: 'All Users', users: result })
-            console.log(result)
         })
         .catch(err => console.log(err))
 })
 
 app.get('/submit', (req, res) => {
     res.render('submit', { title: "Submit User" })
+})
+
+app.get('/update/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(result => {
+            res.render('update', { title: "Update User", user: result })
+        })
+        .catch(err => console.log(err))
 })
 
 app.delete('/delete/:id', (req, res) => {
@@ -46,6 +56,16 @@ app.post('/submit', (req, res) => {
         })
         .catch(err => console.log(err))
     
+})
+
+app.put('/update/:id', (req, res) => {
+    User.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        grade: req.body.grade,
+        section: req.body.section
+    }, { new: true })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))    
 })
 
 
