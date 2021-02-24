3225ddba62bccf629bf38ea2d962dede0fe04209
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2021-02-24 13:17:10 +0800

    fixed css bug

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const routes = require('./routes/routes')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
-app.use(express.static('public'))
+app.use(express.static(__dirname + "/public"))
 
+// "mongodb+srv://kafein:kafeinfaita@cluster0.3xefo.mongodb.net/test-db?retryWrites=true&w=majority"
 
 const dbURI = "mongodb+srv://kafein:kafeinfaita@cluster0.3xefo.mongodb.net/test-db?retryWrites=true&w=majority" || "mongodb://localhost:27017/test-db"
 
